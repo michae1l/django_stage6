4b8ea2b84fd659b1754e974e767b8ff48f08f79f
Michael Esoimeme <mesoimeme4@gmail.com>
2022-06-09 16:36:53 +0100

    second

--- stage6_project/app_stage6/models.py
+++ stage6_project/app_stage6/models.py
@@ -12,20 +12,21 @@ class People(models.Model):
 class Address(models.Model):
     id = models.AutoField(primary_key=True,default="1")
     userName = models.CharField(max_length=510)
+    lga = models.ManyToManyField(People, through="Doctors")
     city = models.CharField(max_length=510)
     state = models.CharField(max_length=510) 
    
 class Doctor(models.Model):
     id = models.AutoField(primary_key=True,default="1")
     name = models.CharField(max_length=510)
-    patient = models.CharField(max_length=510)
+    patient = models.ManyToManyField(People, through="Doctors")
     hospital = models.CharField(max_length=510)
     speciality = models.CharField(max_length=510)
     
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True,default="1")
-    product_name = models.CharField(max_length=510)
+    product_name = models.ManyToManyField(People, )
     product_price = models.CharField(max_length=510)
     product_cat = models.CharField(max_length=510)
     postedby = models.CharField(max_length=510)
